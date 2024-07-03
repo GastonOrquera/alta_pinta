7d05e1f1054295accf66ca8999bd244e7ceed1f8
GastonOrquera <gastonorquera29@gmail.com>
2024-07-03 17:51:39 -0300

    alta_pinta_page

---
+++ main.js
@@ -0,0 +1,15 @@
+ScrollReveal().reveal('.box_producto', {
+    origin: 'right',
+    distance: '10px',
+    duration: 800,
+    easing: 'cubic-bezier(0.5,0,0,1)',
+    interval: 400
+});
+
+ScrollReveal().reveal('h2', {
+    origin: 'top',
+    distance: '14px',
+    duration: 600,
+    easing: 'cubic-bezier(0.5,0,0,1)',
+    interval: 400
+});
